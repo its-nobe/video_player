8a109a98a988e3f74628d996ae5d580f38939407
Abhay <aabhay_rana_@outlook.com>
2023-01-29 03:08:37 +0530

    mono vids

---
+++ src/components/env.js
@@ -0,0 +1,6 @@
+export { default as NavBar } from "./NavBar";
+export { default as Feed } from "./Feed";
+export { default as ChannelDetail } from "./ChannelDetail";
+export { default as SearchField } from "./SearchField";
+export { default as VideoDetail } from "./VideoDetail";
+export { default as SearchBar } from "./SearchBar";
